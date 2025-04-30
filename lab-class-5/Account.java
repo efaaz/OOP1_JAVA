f739ba0daa8ab98c429ced99c69ff4c459ba708d
Wasifur Rahman Efaz <110248681+efaaz@users.noreply.github.com>
2025-04-30 09:57:17 +0600

    add lab class 5

---
+++ lab-class-5/Account.java
@@ -0,0 +1,45 @@
+public class Account {
+    private int accountNum;
+    private String holderName;
+    private double balance;
+
+    public Account() {
+        System.out.print("");
+    }
+
+    public Account(int accountNum, String holderName, double balance) {
+        this.accountNum = accountNum;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    public void setAccountNum(int accountNum) {
+        this.accountNum = accountNum;
+    }
+
+    public void setAccountHolderName(String holderName) {
+        this.holderName = holderName;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public int getAccountNum() {
+        return accountNum;
+    }
+
+    public String getAccountHolderName() {
+        return holderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void showDetails() {
+        System.out.println("Account holder name is: " + holderName);
+        System.out.println("Account number is: " + accountNum);
+        System.out.println("Accout balance is: " + balance);
+    }
+}
